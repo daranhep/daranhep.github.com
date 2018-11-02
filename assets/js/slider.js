e6a080e3fdd487dc6820364a891cff3d6a1b38e9
Dara Nhep <dara.h.nhep@gmail.com>
2018-11-01 21:07:34 -0500

    dafad
    adsfas

---
+++ assets/js/slider.js
@@ -0,0 +1,9 @@
+$('agallery').featherlightGallery({
+    previousIcon: '&#9664;',
+    /* Code that is used as previous icon */
+    nextIcon: '&#9654;',
+    /* Code that is used as next icon */
+    galleryFadeIn: 100,
+    /* fadeIn speed when slide is loaded */
+    galleryFadeOut: 300 /* fadeOut speed before slide is loaded */
+});
